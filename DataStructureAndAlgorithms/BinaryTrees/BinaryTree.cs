ae6a12bdee601db941fe85631b2e41fb2b28c56a
Farshad <farshaddelavarpour@gmail.com>
2024-03-10 13:48:22 +0330

    [feat] Add BinaryTree base implementation

---
+++ DataStructureAndAlgorithms/BinaryTrees/BinaryTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithms.BinaryTrees
+{
+    public class BinaryTree
+    {
+        private Node _root;
+
+        public void Add(int value)
+        {
+            Node node = new(value);
+            if (_root == null)
+            {
+                _root = node;
+                return;
+            }
+
+            Node current = _root;
+            while (true)
+            {
+                if(current.Value < value)
+                {
+                    if(current.Left == null)
+                    {
+                        current.Left = node;
+                        break;
+                    }
+                    current = current.Left;
+                }
+                else
+                {
+                    if(current.Right == null)
+                    {
+                        current.Right = node;
+                        break;
+                    }
+                    current = current.Right;
+                }
+            }
+        }
+
+        public bool Find(int value)
+        {
+            Node current = _root;
+            while(current != null)
+            {
+                if(current.Value == value)
+                {
+                    return true;
+                }
+                if(current.Value < value)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                }
+            }
+            return false;
+        }
+
+
+        private class Node
+        {
+            public override string ToString()
+            {
+                return Value.ToString();
+            }
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+    }
+
+}
