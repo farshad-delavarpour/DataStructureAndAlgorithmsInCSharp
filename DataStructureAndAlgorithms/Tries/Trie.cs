6d1ad7214ae63d308923810950740e41b3e3a161
Farshad <farshaddelavarpour@gmail.com>
2024-03-11 19:34:43 +0330

    [feat] Add Trie base class and insert

---
+++ DataStructureAndAlgorithms/Tries/Trie.cs
@@ -0,0 +1,44 @@
+﻿namespace DataStructureAndAlgorithms.Tries
+{
+    public class Trie
+    {
+        private TrieNode Root { get; set; }
+
+        public void Insert(string value)
+        {
+            TrieNode current = Root;
+            foreach(char @char in value)
+            {
+                if(!current.HasChild(@char))
+                    current.AddChild(@char);
+                current = current.GetChild(@char);
+            }
+            current.IsEndOfWord = true;
+        }
+
+        private class TrieNode
+        {
+            public TrieNode(char value)
+            {
+                Value = value;
+            }
+
+            public char Value { get; set; }
+            public Dictionary<char, TrieNode> Children { get; set; }
+            public bool IsEndOfWord { get; set; }
+
+            public override string ToString()
+            {
+                return Value.ToString();
+            }
+
+            public bool IsEmpty() => !Children.Any();
+
+            public bool HasChild(char value) => Children.ContainsKey(value);
+
+            public TrieNode GetChild(char value) => Children[value];
+
+            public void AddChild(char value) => Children.Add(value, new TrieNode(value));
+        }
+    }
+}
